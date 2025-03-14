12706f00b36b8975709659b850fc09e8b1d06846
Jeremy <jer.auriel@gmail.com>
2025-03-14 10:28:13 +0700

    added landing views

--- src/sigmacrud/app_landing/apps.py
+++ src/sigmacrud/app_landing/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppLandinggConfig(AppConfig):
+class AppLandingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_landingg'
+    name = 'app_landing'

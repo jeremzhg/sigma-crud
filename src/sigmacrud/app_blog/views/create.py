46bd87a1d0cb8cca04011caaf956c900648a5523
Jeremy <jer.auriel@gmail.com>
2025-04-27 17:43:57 +0700

    db fix

--- src/sigmacrud/app_blog/views/create.py
+++ src/sigmacrud/app_blog/views/create.py
@@ -5,6 +5,6 @@ def view(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
-        result = query("INSERT INTO blog_post (title, content) VALUES (%s, %s)", [title, content])
+        result = query("INSERT INTO blog_post (title, content) VALUES (%s, %s)", [title, content]) #
         print(result)
     return render(request, 'app_blog/create.html')

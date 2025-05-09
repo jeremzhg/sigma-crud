cc239a0c01e8533bf9d371955c00115e21c65e4d
Jeremy <jer.auriel@gmail.com>
2025-05-09 10:49:39 +0700

    redirect not found

--- src/sigmacrud/app_blog/views/read.py
+++ src/sigmacrud/app_blog/views/read.py
@@ -4,4 +4,6 @@ from app_blog.utility import query
 def view(request, post_id):
     if request.method == 'GET':
         post = query("SELECT * from blog_post WHERE id = %s", [post_id])
-    return render(request, 'app_blog/read.html', {'post':post[0]})
+        if post:
+            return render(request, 'app_blog/read.html', {'post':post[0]})
+    return render(request, 'app_blog/notfound.html', status=404)
